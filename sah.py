38085a4da48f9d2f9598ff3c576b4c5298300598
yoker5o5 <sirius.djordje@gmail.com>
2020-12-07 13:37:00 +0100

    pomjeranje figura

--- sah.py
+++ sah.py
@@ -1,39 +1,145 @@
 import sys
 from tkinter import *
-
 class Figura:
     def __init__(self, pos, type, boja):
         self.pos = pos
         self.type = type
         self.boja = boja
+        global B
+        B = [[0]*8]*8
+        for i in range(8):
+            B[i] = [0]*8
+        self.createbutton()
 
-    # def moves(self):
-    #     for i in range(8):
-    #         for j in range(8):
+    def createbutton(self):
+        self.button = Button(window, text = self.type, fg=self.boja, height=5, width=12, command = self.moves, bg="blue") if (self.pos[0] % 2) == 0 and (self.pos[1] % 2) == 1 or (self.pos[0] % 2) == 1 and (self.pos[1] % 2) == 0 else Button(window, text = self.type, fg=self.boja, height=5, width=12, command = self.moves)
+        self.button.grid(row=self.pos[0], column=self.pos[1])
 
-    def move(self):
-        if self.boja == "black":
-            self.pos[0] += 1
-        else:
-            self.pos[0] -=1
-        page1()
-
+    def moves(self):
+        global B
+        for i in range(8):
+            for j in range(8):
+                if type(B[i][j]) != int:
+                    B[i][j].destroy()
+        x = self.pos[0]
+        y = self.pos[1]
+        if self.type == "piun":
+            j = self.pos[1]
+            if self.boja == "black":
+                i = self.pos[0]
+                if i == 1:
+                    B[i+2][j] = Button(window, text = self.type, fg=self.boja, height=5, width=12, command = lambda x=[i+2, j]: self.move(x), bg="red")
+                    B[i+2][j].grid(row=i+2, column=j)
+                i += 1
+            else: 
+                i = self.pos[0]
+                if i == 6:
+                    B[i-2][j] = Button(window, text = self.type, fg=self.boja, height=5, width=12, command = lambda x=[i-2, j]: self.move(x), bg="red")
+                    B[i-2][j].grid(row=i-2, column=j)
+                i -= 1
+            B[i][j] = Button(window, text = self.type, fg=self.boja, height=5, width=12, command = lambda x=[i, j]: self.move(x), bg="red")
+            B[i][j].grid(row=i, column=j)
+        elif self.type == "top":
+            for i in range(8):
+                if x != i:
+                    B[i][y] = Button(window, text = self.type, fg=self.boja, height=5, width=12, command = lambda x=[i, y]: self.move(x), bg="red")
+                    B[i][y].grid(row=i, column=y)
+            for j in range(8):
+                if j != y:
+                    B[x][j] = Button(window, text = self.type, fg=self.boja, height=5, width=12, command = lambda x=[x, j]: self.move(x), bg="red")
+                    B[x][j].grid(row=x, column=j)
+        elif self.type == "lovac":
+            for i in range(8):
+                for j in range(8):
+                    if i-x == j-y and [i, j] != [x, y]:
+                        B[i][j] = Button(window, text = self.type, fg=self.boja, height=5, width=12, command = lambda x=[i, j]: self.move(x), bg="red")
+                        B[i][j].grid(row=i, column=j)
+                    elif ((i-x) + (j-y)) == 0 and [i, j] != [x, y]:
+                        B[i][j] = Button(window, text = self.type, fg=self.boja, height=5, width=12, command = lambda x=[i, j]: self.move(x), bg="red")
+                        B[i][j].grid(row=i, column=j)
+        elif self.type == "konj":
+            xx = [x+2, x-2]
+            yy = [y-1, y+1]
+            xxx = [x+1, x-1]
+            yyy = [y+2, y-2]
+            for i in xx:
+                for j in yy:
+                    try:
+                        B[i][j] = Button(window, text = self.type, fg=self.boja, height=5, width=12, command = lambda x=[i, j]: self.move(x), bg="red")
+                        B[i][j].grid(row=i, column=j)
+                    except:
+                        pass
+            for i in xxx:
+                for j in yyy:
+                    try:
+                        B[i][j] = Button(window, text = self.type, fg=self.boja, height=5, width=12, command = lambda x=[i, j]: self.move(x), bg="red")
+                        B[i][j].grid(row=i, column=j)
+                    except:
+                        pass
+        elif self.type == "kralj":
+            xx = [x+1, x-1, x]
+            yy = [y+1, y-1, y]
+            for i in xx:
+                for j in yy:
+                    if i != x or j != y:
+                        try:
+                            B[i][j] = Button(window, text = self.type, fg=self.boja, height=5, width=12, command = lambda x=[i, j]: self.move(x), bg="red")
+                            B[i][j].grid(row=i, column=j)
+                        except:
+                            pass
+        elif self.type == "kraljica":
+            for i in range(8):
+                if x != i:
+                    B[i][y] = Button(window, text = self.type, fg=self.boja, height=5, width=12, command = lambda x=[i, y]: self.move(x), bg="red")
+                    B[i][y].grid(row=i, column=y)
+            for j in range(8):
+                if j != y:
+                    B[x][j] = Button(window, text = self.type, fg=self.boja, height=5, width=12, command = lambda x=[x, j]: self.move(x), bg="red")
+                    B[x][j].grid(row=x, column=j)
+            for i in range(8):
+                for j in range(8):
+                    if i-x == j-y and [i, j] != [x, y]:
+                        B[i][j] = Button(window, text = self.type, fg=self.boja, height=5, width=12, command = lambda x=[i, j]: self.move(x), bg="red")
+                        B[i][j].grid(row=i, column=j)
+                    elif ((i-x) + (j-y)) == 0 and [i, j] != [x, y]:
+                        B[i][j] = Button(window, text = self.type, fg=self.boja, height=5, width=12, command = lambda x=[i, j]: self.move(x), bg="red")
+                        B[i][j].grid(row=i, column=j)
+    def move(self, x):
+        self.button.destroy()
+        self.pos = x
+        self.createbutton()
+        for i in range(8):
+            for j in range(8):
+                if type(B[i][j]) != int:
+                    B[i][j].destroy()
+        
 
 def main(*args):
     global root, figure
     global window
+    root = Tk()
+    root.title("Sah")
+    window = Frame(root,bg='lightblue')
+    page1()
     figure = []
-    figure.append(Figura([0, 0], "top", "black"))
+    for i in [0, 7]:
+        figure.append(Figura([0, i], "top", "black"))
+        figure.append(Figura([7, i], "top", "white"))
+    for i in [1,6]:
+        figure.append(Figura([0, i], "konj", "black"))
+        figure.append(Figura([7, i], "konj", "white"))
+    for i in [2, 5]:
+        figure.append(Figura([0, i], "lovac", "black"))
+        figure.append(Figura([7, i], "lovac", "white"))
     for i in range(8):
         figure.append(Figura([1, i], "piun", "black"))
     for i in range(8):
         figure.append(Figura([6, i], "piun", "white"))
-    figure.append(Figura([0, 3], "k", "black"))
-    root = Tk()
+    figure.append(Figura([0, 4], "kralj", "black"))
+    figure.append(Figura([0, 3], "kraljica", "black"))
+    figure.append(Figura([7, 4], "kralj", "white"))
+    figure.append(Figura([7, 3], "kraljica", "white"))
 
-    root.title("Sah")
-    window = Frame(root,bg='lightblue')
-    page1()
     root.mainloop()
 
 def page1():
@@ -42,18 +148,13 @@ def page1():
 
     B = [[0]*8]*8
 
-    figpos = []
-    for fig in figure:
-        for i, col in enumerate(B):
-            #B[i] = [0]*8
-            for j, _ in enumerate(col):
-                if (i % 2) == 0 and (j % 2) == 1 or (i % 2) == 1 and (j % 2) == 0:
-                    B[i][j] = Button(window, text = fig.type, fg=fig.boja, height=5, width=12, command = fig.move, bg="red") if [i, j] == fig.pos else Button(window, text = " ", height=5, width=12, bg="red")
-                else:
-                    B[i][j] = Button(window, text = fig.type, fg=fig.boja, height=5, width=12, command = fig.move) if [i, j] == fig.pos else Button(window, text = " ", height=5, width=12)
-                if [i, j] not in figpos:
-                    B[i][j].grid(row=i, column=j)
-        figpos.append(fig.pos)
+    for i, col in enumerate(B):
+        for j, _ in enumerate(col):
+            if (i % 2) == 0 and (j % 2) == 1 or (i % 2) == 1 and (j % 2) == 0:
+                B[i][j] = Label(window, text = " ", height=5, width=12, bg="blue")
+            else:
+                B[i][j] = Label(window, text = " ", height=5, width=12)
+            B[i][j].grid(row= i, column=j)
 
 if __name__ == '__main__':
     _, *script_args = sys.argv
